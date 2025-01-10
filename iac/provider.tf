cb1b25f0b68cfac1ea8e949baa8206c615408941
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2025-01-10 10:42:02 +0100

    Migrating to ARM-variables

--- iac/provider.tf
+++ iac/provider.tf
@@ -11,8 +11,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  tenant_id       = "bf34d03a-c205-4d66-be38-2905964b5c5b"
-  subscription_id = "d1bbe07c-713a-4149-8c8e-313060b62dd0"
-  client_id       = "6eac731a-8b06-403f-8f44-8fc0f54cf642"
-  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
 }
--- iac/variables.tf
+++ iac/variables.tf
@@ -1,4 +1,5 @@
-variable "client_secret" {
-  description = "The Azure client secret for the client ID"
+variable "subscription_id" {
+  description = "The Azure subscription ID"
   type        = string
+  default     = "d1bbe07c-713a-4149-8c8e-313060b62dd0"
 }
