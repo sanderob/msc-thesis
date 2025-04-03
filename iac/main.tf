d01fdfe67c1821b88428e952c17b49c043b52c0d
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2025-04-03 15:28:53 +0200

    Remove registration

--- iac/main.tf
+++ iac/main.tf
@@ -2,12 +2,3 @@ resource "azurerm_resource_group" "msc-rg" {
   name     = "msc-prod-rg"
   location = "Norway East"
 }
-
-resource "azurerm_resource_provider_registration" "peering-feature" {
-  name = "Microsoft.Network"
-
-  feature {
-    name       = "AllowMultiplePeeringLinksBetweenVnets"
-    registered = true
-  }
-}
