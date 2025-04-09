9024c57ddfef19a73d09da29791a915cd8e1cbc1
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2025-04-09 09:59:28 +0200

    Terraform fmt

--- iac/poc-local.tf
+++ iac/poc-local.tf
@@ -22,7 +22,7 @@ resource "azurerm_network_security_group" "poc-local-vnet-nsg" {
   location            = azurerm_resource_group.msc-rg.location
   resource_group_name = azurerm_resource_group.msc-rg.name
 
-    security_rule {
+  security_rule {
     name                         = "DenySubnet"
     priority                     = 1000
     direction                    = "Outbound"
