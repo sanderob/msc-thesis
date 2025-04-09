b4281a47a33eaccf606f162bc43eac85b2e05369
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2025-04-09 14:17:57 +0200

    Added gteway and gateway connection for poc-local

--- iac/poc-local.tf
+++ iac/poc-local.tf
@@ -126,3 +126,24 @@ resource "azurerm_virtual_network_gateway" "poc-local-vnet-gateway" {
     subnet_id                     = azurerm_subnet.poc-local-subnet-1.id
   }
 }
+
+resource "azurerm_local_network_gateway" "poc-local-local-gateway" {
+  name                = "fortigate-local-gateway"
+  resource_group_name = azurerm_resource_group.msc-rg.name
+  location            = azurerm_resource_group.msc-rg.location
+  gateway_address     = "77.106.154.138"
+  address_space       = ["10.3.0.0/16"]
+}
+
+
+resource "azurerm_virtual_network_gateway_connection" "poc-local-fortigate-local-connection" {
+  name                = "fortigate-local-connection"
+  location            = azurerm_resource_group.msc-rg.location
+  resource_group_name = azurerm_resource_group.msc-rg.name
+
+  type                       = "IPsec"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.poc-local-vnet-gateway.id
+  local_network_gateway_id   = azurerm_local_network_gateway.poc-local-local-gateway.id
+
+  shared_key = var.poc-local-vpn-shared-key
+}
