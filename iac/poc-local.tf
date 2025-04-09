b2e712660a41e18d0955067c4c24fdcc3a5e8aa3
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2025-04-09 09:33:32 +0200

    Added public IP

--- iac/poc-local.tf
+++ iac/poc-local.tf
@@ -27,3 +27,12 @@ resource "azurerm_subnet_network_security_group_association" "poc-local-subnet-1
   subnet_id                 = azurerm_subnet.poc-local-subnet-1.id
   network_security_group_id = azurerm_network_security_group.poc-local-vnet-nsg.id
 }
+
+resource "azurerm_public_ip" "poc-local-public-ip" {
+  name                = "poc-local-ip"
+  location            = azurerm_resource_group.msc-rg.location
+  resource_group_name = azurerm_resource_group.msc-rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  ip_version          = "IPv4"
+}
