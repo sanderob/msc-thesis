8b9d60d2aeca6ea20a427c2ca2c6cd1e0b980675
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2025-04-10 14:36:29 +0200

    Added use of modules

--- iac/main.tf
+++ iac/main.tf
@@ -2,3 +2,16 @@ resource "azurerm_resource_group" "msc-rg" {
   name     = "msc-prod-rg"
   location = "Norway East"
 }
+
+module "core" {
+  source = "./core"
+}
+
+module "poc-cloud" {
+  source = "./poc-local"
+}
+
+module "poc-cloud" {
+  source = "./poc-cloud"
+  poc-local-vpn-shared-key = module.poc-local.poc-local-vpn-shared-key
+}
