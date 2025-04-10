c2b0c2bb1baa378ca2130c1ebe6b3a675b7a28d4
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2025-04-10 14:38:54 +0200

    Fix typo

--- iac/main.tf
+++ iac/main.tf
@@ -7,11 +7,11 @@ module "core" {
   source = "./core"
 }
 
-module "poc-cloud" {
+module "poc-local" {
   source = "./poc-local"
+  poc-local-vpn-shared-key = module.poc-local.poc-local-vpn-shared-key
 }
 
 module "poc-cloud" {
   source                   = "./poc-cloud"
-  poc-local-vpn-shared-key = module.poc-local.poc-local-vpn-shared-key
 }
