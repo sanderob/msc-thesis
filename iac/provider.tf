5393866abb1937ef70a93f2a978d3c7e7d91d54e
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2025-05-02 15:32:35 +0200

    Upgrade provider

--- iac/provider.tf
+++ iac/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.16"
+      version = "~> 4.27"
     }
   }
   backend "azurerm" {}
-  required_version = "~> 1.11.2"
+  required_version = "~> 1.11.4"
 }
 
 provider "azurerm" {
